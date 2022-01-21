9c57ec491f2f33c94cb34abbc36b0b59281c15e1
elvinkarimli <95429608+k97elvin@users.noreply.github.com>
2022-01-21 12:35:22 -0500

    Initial commit

---
+++ src/Day41_Exceprions/TryCatchBlocks.java
@@ -0,0 +1,39 @@
+package Day41_Exceprions;
+
+import javax.xml.transform.Source;
+
+public class TryCatchBlocks {
+    public static void main(String[] args) {
+        System.out.println("Test started");
+        try {
+            System.out.println(9 / 0);
+            System.out.println("Try Block");
+        } catch (ArithmeticException e) {
+            System.out.println("Catch Block");
+            System.out.println("Arithmetic Exception is occurred");
+        }
+        System.out.println("Test 1 is completed");
+
+        System.out.println("___________________________________");
+
+
+        int[] numbers = {1, 2, 3, 4, 5};
+       try {
+           System.out.println(numbers[200]);
+       }catch (Exception e){
+        e.printStackTrace();
+       }
+        System.out.println("Test 2 completed");
+
+        System.out.println("_______________________________________________________");
+
+
+        try {
+            System.out.println();
+        }catch (Error e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
